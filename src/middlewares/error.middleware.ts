814e1134c7965872cdd0d5a66972ef326451b4fc
Jayash Raj Singh Yadav <65907404+JayashRSY@users.noreply.github.com>
2025-06-08 22:19:27 +0530

    Update error.middleware.ts

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,45 +1,31 @@
 import logger from '../utils/logger.ts';
 import { NextFunction, Request, Response } from "express";
-
-class ApiError extends Error {
-  statusCode: number;
-  isOperational: boolean;
-  constructor(statusCode: number, message: string, isOperational = true, stack = '') {
-    super(message);
-    this.statusCode = statusCode;
-    this.isOperational = isOperational;
-    if (stack) {
-      this.stack = stack;
-    } else {
-      Error.captureStackTrace(this, this.constructor);
-    }
-  }
-}
+import ApiError from '../utils/ApiError.ts';
 
 const errorMiddleware = (err: Error, req: Request, res: Response, next: NextFunction) => {
     console.log("🚀 ~ errorMiddleware ~ err:", err)
-    let statusCode = (err as ApiError).statusCode || 500;
-    let message = (err as ApiError).message || 'Internal Server Error';
-    if(!(err instanceof ApiError)){
-        statusCode = 500
-        message = "Internal Server Error";
-    }
-  logger.error({
-    message: 'Error occurred',
-    error: err,
-    stack: err.stack,
-    url: req.url,
-    method: req.method,
-    body: req.body,
-  });
+    let statusCode = 500;
+    let message = 'Internal Server Error';
 
-  res.status(statusCode).json({
-    error: {
-      message: message,
-    },
-  });
-  return
+    if (err instanceof ApiError) {
+        statusCode = err.statusCode;
+        message = err.message;
+    }
+
+    logger.error({
+        message: 'Error occurred',
+        error: err,
+        stack: err.stack,
+        url: req.url,
+        method: req.method,
+        body: req.body,
+    });
+
+    res.status(statusCode).json({
+        error: {
+            message: message,
+        },
+    });
 };
 
 export default errorMiddleware;
-export {ApiError};
